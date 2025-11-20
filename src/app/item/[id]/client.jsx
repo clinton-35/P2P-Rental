d75bab34adbe4230a7dec99db5f0f33b6f6e48c9
clinton-35 <clinton.wambua@student.moringaschool.com>
2025-11-20 09:39:36 +0300

    Changing the currency type to avoid clint-server mismatch

--- src/app/item/[id]/client.jsx
+++ src/app/item/[id]/client.jsx
@@ -23,8 +23,8 @@ const renderPrice = (item) => {
   if (item.price.type === "Fixed" || item.price.type === "Negotiable") {
     return (
       <>
-        <span className={`font-[600] text-gray-700`}>KES. </span>
-        {item.price.amount.toLocaleString("ne-NP")}
+        <span className="font-[600] text-gray-700">KES. </span>
+        {item.price.amount.toLocaleString("en-KE")}
       </>
     );
   } else {
@@ -39,7 +39,7 @@ const renderDeliveryCost = (cost) => {
     return (
       <>
         <span className={`font-[600] text-gray-700`}>KES. </span>
-        {cost.toLocaleString("ne-NP")}
+        {cost.toLocaleString("en-KE")}
       </>
     );
   }
@@ -55,11 +55,9 @@ export default function ItemClient({ item, related }) {
   useEffect(() => {
     if (session.status === "authenticated") {
       setChatIcon("majesticons:chat-line");
-    }
-    else if (session.status === "unauthenticated") {
+    } else if (session.status === "unauthenticated") {
       setChatIcon("material-symbols:lock-outline");
-    }
-    else {
+    } else {
       setChatIcon("eva:loader-outline");
     }
   }, [session]);
@@ -69,7 +67,7 @@ export default function ItemClient({ item, related }) {
       router.push(`/edit/${item._id}`);
       return;
     }
-    
+
     if (session.status === "authenticated") {
       setChatLoading(true);
       const data = {
@@ -78,23 +76,23 @@ export default function ItemClient({ item, related }) {
         seller_id: item.seller,
       };
 
-      axios.post("/api/chats/create", data)
-      .then((res) => {
-        if (res.status === 200) {
-          router.push(`/message/${res.data._id}`);
-        }
-      })
-      .catch((err) => {
-        setChatLoading(false);
-        Swal.fire({
-          title: "Error",
-          text: err.response.data.error,
-          icon: "error",
-          confirmButtonText: "OK",
+      axios
+        .post("/api/chats/create", data)
+        .then((res) => {
+          if (res.status === 200) {
+            router.push(`/message/${res.data._id}`);
+          }
+        })
+        .catch((err) => {
+          setChatLoading(false);
+          Swal.fire({
+            title: "Error",
+            text: err.response.data.error,
+            icon: "error",
+            confirmButtonText: "OK",
+          });
         });
-      });
-    }
-    else if (session.status === "unauthenticated") {
+    } else if (session.status === "unauthenticated") {
       Swal.fire({
         title: "Login Required",
         text: "You must be logged in to chat with the seller.",
@@ -103,60 +101,89 @@ export default function ItemClient({ item, related }) {
         confirmButtonText: "Login",
         cancelButtonText: "Cancel",
         reverseButtons: true,
-      })
-      .then((result) => {
+      }).then((result) => {
         if (result.isConfirmed) {
           window.location.href = "/login";
         }
       });
     }
-  }
+  };
 
   return (
     <>
       <div className="flex flex-col md:flex-row max-w-screen-xl mx-auto mt-[80px]">
         <div className={`flex flex-col md:w-1/2 m-4 relative rounded-xl`}>
-          <ImageCarousel images={item.images} activeImageIndex={activeImageIndex} setActiveImageIndex={setActiveImageIndex} />
+          <ImageCarousel
+            images={item.images}
+            activeImageIndex={activeImageIndex}
+            setActiveImageIndex={setActiveImageIndex}
+          />
           <div className="my-4 flex justify-between">
             <span className={`font-[500] flex items-center`}>
-              <Icon icon="tabler:eye" width={24} height={24} className={`mr-1`} />
+              <Icon
+                icon="tabler:eye"
+                width={24}
+                height={24}
+                className={`mr-1`}
+              />
               {item.views} Views
             </span>
             <button
-              disabled={chatLoading || item.status === "sold" && !item.selfOwned}
-              className={`font-[500] flex items-center text-white px-4 py-2 rounded-xl ${(chatLoading || item.status === "sold" && !item.selfOwned) ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"}`}
-              onClick={chatButtonClicked}>
-              {
-                item.selfOwned ? (
-                  <>
-                    <Icon icon="bx:edit" width={24} height={24} className={`mr-1`} />
-                    Edit Item
-                  </>
-                ) : (
-                  <>
-                    {
-                      item.status === "sold" ? (
-                        <>
-                          <Icon icon="bx:bx-check" width={24} height={24} className={`mr-1`} />
-                          On Loan
-                        </>
-                      ) : (
-                        chatLoading ? (
-                          <>
-                            <Icon icon="eva:loader-outline" width={24} height={24} className={`mr-1 animate-spin`} />
-                            Opening Chat
-                          </>
-                        ) : (
-                          <>
-                            <Icon icon={chatIcon} width={24} height={24} className={`mr-1`} />
-                            Chat With Seller
-                          </>
-                        )
-                      )
-                    }
-                  </>
-                )
+              disabled={
+                chatLoading || (item.status === "sold" && !item.selfOwned)
               }
+              className={`font-[500] flex items-center text-white px-4 py-2 rounded-xl ${
+                chatLoading || (item.status === "sold" && !item.selfOwned)
+                  ? "bg-gray-400 cursor-not-allowed"
+                  : "bg-blue-500 hover:bg-blue-600"
+              }`}
+              onClick={chatButtonClicked}
+            >
+              {item.selfOwned ? (
+                <>
+                  <Icon
+                    icon="bx:edit"
+                    width={24}
+                    height={24}
+                    className={`mr-1`}
+                  />
+                  Edit Item
+                </>
+              ) : (
+                <>
+                  {item.status === "sold" ? (
+                    <>
+                      <Icon
+                        icon="bx:bx-check"
+                        width={24}
+                        height={24}
+                        className={`mr-1`}
+                      />
+                      On Loan
+                    </>
+                  ) : chatLoading ? (
+                    <>
+                      <Icon
+                        icon="eva:loader-outline"
+                        width={24}
+                        height={24}
+                        className={`mr-1 animate-spin`}
+                      />
+                      Opening Chat
+                    </>
+                  ) : (
+                    <>
+                      <Icon
+                        icon={chatIcon}
+                        width={24}
+                        height={24}
+                        className={`mr-1`}
+                      />
+                      Chat With Seller
+                    </>
+                  )}
+                </>
+              )}
             </button>
           </div>
         </div>
@@ -174,16 +201,27 @@ export default function ItemClient({ item, related }) {
             </thead>
             <tbody>
               <TableRow label="Price" value={renderPrice(item)} />
-              <TableRow label="Delivery Area" value={deliveryType[item.delivery.type].area} />
-              <TableRow label="Delivery Type" value={deliveryType[item.delivery.type].type} />
+              <TableRow
+                label="Delivery Area"
+                value={deliveryType[item.delivery.type].area}
+              />
+              <TableRow
+                label="Delivery Type"
+                value={deliveryType[item.delivery.type].type}
+              />
               {item.delivery.type !== "Door Pickup" && (
-                <TableRow label="Delivery Cost" value={renderDeliveryCost(item.delivery.cost)} />
+                <TableRow
+                  label="Delivery Cost"
+                  value={renderDeliveryCost(item.delivery.cost)}
+                />
               )}
               <TableRow
-                label="Sold By"
+                label="Owned By"
                 value={
                   <Link className="flex gap-2" href={`/lists/${item.seller}`}>
-                    <span className={`font-[600] text-gray-800 flex items-center gap-1 hover:text-red-500 cursor-pointer`}>
+                    <span
+                      className={`font-[600] text-gray-800 flex items-center gap-1 hover:text-red-500 cursor-pointer`}
+                    >
                       {item.seller_name}
                       <Icon icon="ic:round-link" width={24} height={24} />
                     </span>
@@ -216,32 +254,34 @@ export default function ItemClient({ item, related }) {
       <div className="max-w-screen-xl mx-auto mt-[30px]">
         <h1 className="text-2xl font-bold mx-4">You Might Also Like</h1>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-4">
-          {
-            related.map((item, index) => (
-              <Link key={index} href={`/item/${item._id}`}>
-                <div className="flex flex-col rounded-xl overflow-hidden shadow-md cursor-pointer hover:shadow-lg h-full">
-                  <div className="relative">
-                    <Image
-                      src={"https://wsrv.nl?url=" + item.images[0] + "&w=200&h=200&fit=cover&a=attention"}
-                      alt={item.name}
-                      className="h-full w-full object-cover hover:scale-105 transition-all duration-300"
-                      height={200}
-                      width={200}
-                    />
-                  </div>
-                  <div className="p-4">
-                    <h1 className="text-xl font-bold">{item.name}</h1>
-                    <span className="text-gray-600">{item.category}</span>
-                    <div className="flex items-center gap-2 mt-2">
-                      {renderPrice(item)}
-                    </div>
+          {related.map((item, index) => (
+            <Link key={index} href={`/item/${item._id}`}>
+              <div className="flex flex-col rounded-xl overflow-hidden shadow-md cursor-pointer hover:shadow-lg h-full">
+                <div className="relative">
+                  <Image
+                    src={
+                      "https://wsrv.nl?url=" +
+                      item.images[0] +
+                      "&w=200&h=200&fit=cover&a=attention"
+                    }
+                    alt={item.name}
+                    className="h-full w-full object-cover hover:scale-105 transition-all duration-300"
+                    height={200}
+                    width={200}
+                  />
+                </div>
+                <div className="p-4">
+                  <h1 className="text-xl font-bold">{item.name}</h1>
+                  <span className="text-gray-600">{item.category}</span>
+                  <div className="flex items-center gap-2 mt-2">
+                    {renderPrice(item)}
                   </div>
                 </div>
-              </Link>
-            ))
-          }
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
